c0b9ff4cb7c83ccdad4ffcef3c0583ce71b3e7b5
Nick ten Veen <nveen0930@gmail.com>
2017-06-11 10:12:34 +0200

    dot source did not take up full height

--- src/main/js/components/DotContainer.js
+++ src/main/js/components/DotContainer.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import {dotformat} from '../selectors';
 
 const DotContainer = ({dot}) => {
-  return dot !== null ? <pre>{dot}</pre> : <div></div>;
+  return dot !== null ? <pre className="dot-source-container">{dot}</pre> : <div></div>;
 };
 
 function mapStateToProps(state){
