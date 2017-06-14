7bd63378f03b22ea844ceb2e94a6c3e2e3b08574
Nick ten Veen <nveen0930@gmail.com>
2017-06-14 13:09:56 +0200

    moved webapp to separate folder

--- build-pages.sh
+++ build-pages.sh
@@ -7,9 +7,9 @@ if [ -d "$BUILD_DIR" ]; then
 fi
 
 if hash yarn 2>/dev/null; then
-  JS_BUILD_CMD=yarn
+  JS_BUILD_CMD="yarn"
 elif hash npm 2>/dev/null; then
-  JS_BUILD_CMD=npm
+  JS_BUILD_CMD="npm run-script"
 else
   echo "yarn or npm is required to build"
   exit 1
@@ -21,6 +21,9 @@ if ! hash sbt 2>/dev/null; then
 fi
 
 sbt clean fullOptJS
+cd webapp
 $JS_BUILD_CMD build
+cd ..
 mkdir -p $BUILD_DIR
-find target -maxdepth 1 -regex ".*\.\(js\|svg\|woff\|woff2\|ttf\|eot\)" | xargs -i cp {} $BUILD_DIR
+cp webapp/target/* $BUILD_DIR
+#find target -maxdepth 1 -regex ".*\.\(js\|svg\|woff\|woff2\|ttf\|eot\)" | xargs -i cp {} $BUILD_DIR
